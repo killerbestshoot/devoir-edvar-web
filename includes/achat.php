f73afcb0227d6b0988e5b1eac368b5309232fff4
Bastien Yvenord Jean Nance <killerbestshoot@gmail.com>
2022-08-30 21:07:47 -0400

    Update achat.php
    intrgration de l interface achat

---
+++ includes/achat.php
@@ -0,0 +1,24 @@
+<div>
+    <div>
+        <h2>Achats</h2>
+    </div>
+    <form method="post">
+    <label for=´N-client´>No clients</label>
+    <input type=´text´ name=´N-clients´ placeholder=´Ex : ´>
+    
+    <label for=´N-article´>Numero article</label>
+    <input type=´text´ name=´N-article´ placeholder=´Ex : ´>
+    
+    <label for=´Nb-article´>Nombre article</label>
+    <input type=´text´ name=´Nb-article´ placeholder=´Ex : ´>
+    
+    <label for=´a-date´>Date achat</label>
+    <input type=´text´ name=´a-date´ placeholder=´Ex : ´>
+    
+    <label for=´P-achat´>Prix Achat</label>
+    <input type=´text´ name=´P-achat´ placeholder=´Ex : ´>
+    
+        <input type="submit" value="effacer">
+        <input type="submit" value="Ajouter">
+    </form>
+</div>
