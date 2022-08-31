c1d2f7d628b8360aca2c66489242a24972fe27e3
Bastien Yvenord Jean Nance <killerbestshoot@gmail.com>
2022-08-30 21:10:37 -0400

    Update achat.php
    amelioration

--- includes/achat.php
+++ includes/achat.php
@@ -4,19 +4,19 @@
     </div>
     <form method="post">
     <label for=´N-client´>No clients</label>
-    <input type=´text´ name=´N-clients´ placeholder=´Ex : ´>
+    <input type=´text´ name=´N-clients´ placeholder=´Ex : CL-2347 ´>
     
     <label for=´N-article´>Numero article</label>
-    <input type=´text´ name=´N-article´ placeholder=´Ex : ´>
+    <input type=´text´ name=´N-article´ placeholder=´Ex : A-2364´>
     
     <label for=´Nb-article´>Nombre article</label>
-    <input type=´text´ name=´Nb-article´ placeholder=´Ex : ´>
+    <input type=´text´ name=´Nb-article´ placeholder=´Ex : 10´>
     
     <label for=´a-date´>Date achat</label>
-    <input type=´text´ name=´a-date´ placeholder=´Ex : ´>
+    <input type=´text´ name=´a-date´ placeholder=´Ex : 12-12-2022´>
     
     <label for=´P-achat´>Prix Achat</label>
-    <input type=´text´ name=´P-achat´ placeholder=´Ex : ´>
+    <input type=´text´ name=´P-achat´ placeholder=´Ex : 1800´>
     
         <input type="submit" value="effacer">
         <input type="submit" value="Ajouter">
