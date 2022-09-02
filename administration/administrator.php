d97ff859da38b324b44511a839271b045b8973e5
Bastien Yvenord Jean Nance <killerbestshoot@gmail.com>
2022-09-02 02:35:36 -0400

    Update administrator.php

--- administration/administrator.php
+++ administration/administrator.php
@@ -1,3 +1,11 @@
 <?php
 session_start();
-$
+$ID= $_SESSION['Auth_user_session']['ID'];
+$POSTE=$_SESSION['Auth_user_session']['postes'];
+$USERNAME=$_SESSION['Auth_user_session']['user_name'];
+$USERMAIL=$_SESSION['Auth_user_session']['user_mail'];
+$NOM=$_SESSION['Auth_user_session']['nom'];
+$PRENOM=$_SESSION['Auth_user_session']['prenom'];
+$TEL=$_SESSION['Auth_user_session']['tel'];
+                   
+include '';
