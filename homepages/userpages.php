51805c26d51d6409abded0e96e683d73aa079892
Bastien Yvenord Jean Nance <killerbestshoot@gmail.com>
2022-09-02 02:41:49 -0400

    Update userpages.php

--- homepages/userpages.php
+++ homepages/userpages.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+$ID= $_SESSION['Auth_user_session']['ID'];
+$POSTE=$_SESSION['Auth_user_session']['postes'];
+$USERNAME=$_SESSION['Auth_user_session']['user_name'];
+$USERMAIL=$_SESSION['Auth_user_session']['user_mail'];
+$NOM=$_SESSION['Auth_user_session']['nom'];
+$PRENOM=$_SESSION['Auth_user_session']['prenom'];
+$TEL=$_SESSION['Auth_user_session']['tel'];
 include '../includes/header.php';
 ?>
 <main>
