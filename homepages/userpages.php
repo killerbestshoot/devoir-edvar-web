594eb4a51f8c74497c4e6f2d84dfa3516366c2fc
killerbestshoot <killerbestshoot@gmail.com>
2022-09-03 00:46:55 -0400

    ajout des codes css dans les page de connections  et boostrap

--- homepages/userpages.php
+++ homepages/userpages.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-$ID= $_SESSION['Auth_user_session']['ID'];
-$POSTE=$_SESSION['Auth_user_session']['postes'];
-$USERNAME=$_SESSION['Auth_user_session']['user_name'];
-$USERMAIL=$_SESSION['Auth_user_session']['user_mail'];
-$NOM=$_SESSION['Auth_user_session']['nom'];
-$PRENOM=$_SESSION['Auth_user_session']['prenom'];
-$TEL=$_SESSION['Auth_user_session']['tel'];
+$ID = $_SESSION['Auth_user_session']['ID'];
+$POSTE = $_SESSION['Auth_user_session']['postes'];
+$USERNAME = $_SESSION['Auth_user_session']['user_name'];
+$USERMAIL = $_SESSION['Auth_user_session']['user_mail'];
+$NOM = $_SESSION['Auth_user_session']['nom'];
+$PRENOM = $_SESSION['Auth_user_session']['prenom'];
+$TEL = $_SESSION['Auth_user_session']['tel'];
 include '../includes/header.php';
 ?>
-<main>
+<main class="body-separate">
     <section class="gauche">
         <?php include '../includes/options.php'; ?>
     </section>
--- includes/header.php
+++ includes/header.php
@@ -5,35 +5,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" href="../css-styles/userpage-css.css">
     <title>Plezi shipping | Vendeur </title>
 </head>
 
 <body>
     <header>
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="nom-entreprise">
-                    <h1>
-                        Plezi shipping
-                    </h1>
+<nav>
+<link rel="stylesheet" href="../css-styles/userpage-css.css" />
+<div class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" data-target="#mobile_menu" data-toggle="collapse">
+                        <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand">Plezi Shipping</a>
                 </div>
-                <form method="get" class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Rechercher ..." aria-label="Search">
-                </form>
-                <div class="dropdown">
-                    <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
-                        id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                        <img src="https://avatars.githubusercontent.com/u/101983731?v=4" class="rounded-circle"
-                            height="40" alt="Black and White Portrait of a Man" loading="lazy" />
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                        <li><a class="dropdown-item" href="">Deconnecter</a></li>
-                        <li><a class="dropdown-item" href="" aria-current="page">Mon profil</a></li>
+
+                <div class="navbar-collapse collapse" id="mobile_menu">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <form action="" class="navbar-form">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input type="search" name="search" id="" placeholder="Rechercher..."
+                                            class="form-control" />
+                                        <span class="input-group-addon"><span
+                                                class="glyphicon glyphicon-search"></span></span>
+                                    </div>
+                                </div>
+                            </form>
+                        </li>
                     </ul>
 
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#">
+                                <img src="https://avatars.githubusercontent.com/u/101983731?s=400&u=1a50431ae2c21b8161f0eba82a238183c8db1350&v=4"
+                                    class="img-circle" height="30" alt="Portrait of a Woman" loading="lazy" />
+                                Profile</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span
+                                    class="glyphicon glyphicon-log-in"></span> Plus
+                                <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Parametre</a></li>
+                                <li><a href="#">Deconnection</a></li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
         </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</nav>
+<!-- Navbar -->
     </header>
