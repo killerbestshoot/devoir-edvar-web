d1147691d91ceb30e1a37035d7527959ef166db3
killerbestshoot <killerbestshoot@gmail.com>
2022-09-04 00:14:25 -0400

    amelioration

--- homepages/userpages.php
+++ homepages/userpages.php
@@ -15,7 +15,18 @@ include '../includes/header.php';
         <?php include '../includes/options.php'; ?>
     </section>
     <section class="droite">
-        <?php include '../includes/clients.php'; ?>
+        <?php 
+        $option_menu=$_GET;
+        switch($option_menu){
+            case $option_menu==="insertio-client":
+                include '../includes/clients.php';
+                break;
+                case $option_menu==="insertion_article";
+                break;
+                default:
+                include '../includes/clients.php';
+
+        }?>
 
     </section>
 </main>
--- includes/options.php
+++ includes/options.php
@@ -2,33 +2,38 @@
 switch ($POSTE) {
     case $POSTE === "vendeur":
         echo "
-<div>
-    
-    <div>
-        <h3>Insertion</h3>
-        <form method=´get´>
-        <li><a href=''>Clients</a></li>
-        <li><a href=''>Articles</a></li>
-        <li><a href=''>Achats</a></li>
-        </form>
+<nav class='navbar navbar-default' role='navigation'>
+  <div class='navbar-header'>
+    <h4 class=''>Insertion</h4>
+  </div>
+
+  <div class='collapse navbar-collapse navbar-ex1-collapse'>
+    <form action=''>
+      <ul class='nav navbar-nav'>
+        <li class='active'><a href='#'>client</a></li>
+        <li><a href='#'>article</a></li>
+        <li><a href='#'>achat</a></li>
+      </ul>
+    </form>
+    <div class='navbar-header'>
+      <h4 class=''>Modiffication</h4>
     </div>
-    <div>
-        <h3>Modidfication</h3>
-        <form method=´get´>
-        <li><a href=''>Clients</a></li>
-        <li><a href=''>Articles</a></li>
-        <li><a href=''>Achats</a></li>
-        </form>
+
+    <ul class='nav navbar-nav'>
+      <li class='active'><a href='#'>client</a></li>
+      <li><a href=''>article</a></li>
+      <li><a href=''>achat</a></li>
+    </ul>
+    <div class='navbar-header'>
+      <h4 class=''>Consultation</h4>
     </div>
-    <div>
-        <h3>Consultation</h3>
-        <form method=´get´>
-        <li><a href=''>Clients</a></li>
-        <li><a href=''>Articles</a></li>
-        <li><a href=''>Achats</a></li>
-        </form>
-    </div>
-</div>";
+    <ul class='nav navbar-nav'>
+      <li class='active'><a href=''>client</a></li>
+      <li><a href=''>article</a></li>
+      <li><a href=''>achat</a></li>
+    </ul>
+  </div>
+</nav>";
         break;
     case $POSTE === "comptable":
         echo "
@@ -36,7 +41,7 @@ switch ($POSTE) {
     
     <div>
         <h3>Insertion</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
@@ -44,7 +49,7 @@ switch ($POSTE) {
     </div>
     <div>
         <h3>Modidfication</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
@@ -52,7 +57,7 @@ switch ($POSTE) {
     </div>
     <div>
         <h3>Consultation</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
@@ -66,7 +71,7 @@ switch ($POSTE) {
     
     <div>
         <h3>Insertion</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
@@ -74,7 +79,7 @@ switch ($POSTE) {
     </div>
     <div>
         <h3>Modidfication</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
@@ -82,7 +87,7 @@ switch ($POSTE) {
     </div>
     <div>
         <h3>Consultation</h3>
-        <form method=´get´>
+        <form method='get'>
         <li><a href=''>Clients</a></li>
         <li><a href=''>Articles</a></li>
         <li><a href=''>Achats</a></li>
