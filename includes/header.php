316d6af5f1d72cfef557bdf82c7253ad7826bdfa
Bastien Yvenord Jean Nance <killerbestshoot@gmail.com>
2022-09-07 21:53:33 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -82,7 +82,6 @@ endif;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" href="../css-styles/userpage-css.css">
     <title>Plezi shipping | Vendeur
